21df857e650d87a84e1e50ba6d5ddd41f221e040
risification <sultangaziev01@bk.ru>
2021-05-09 21:55:13 +0600

    add url - education, warcraft, car

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,10 +5,6 @@ from .views import *
 router = DefaultRouter()
 router.register('dossier', DossierModelViewSet)
 router.register('register', RegisterModelViewSet)
-router.register('education', EducationModelViewSet)
-router.register('warcraft', WarcraftModelViewSet)
-router.register('car', CarModelViewSet)
-
 urlpatterns = [
     path('', include(router.urls)),
 ]
