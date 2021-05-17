2620d752b95ed32c39dcb255f9e7cbd74a818197
risification <sultangaziev01@bk.ru>
2021-05-17 19:45:38 +0600

    add view accounts AuthView

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from .serializers import *
 from rest_framework import viewsets
+from rest_framework.views import APIView
 
 
 # Create your views here.
@@ -11,6 +14,36 @@ class DossierModelViewSet(viewsets.ModelViewSet):
     serializer_class = DossierSerializers
 
 
+# class DossierView(APIView):
+#     def get(self, request, *args, **kwargs):
+#         dossier = Dossier.objects.get(user=request.user)
+#         serializers = DossierSerializers(dossier)
+#         return Response(serializers.data)
+#
+#     def put(self, request, *args, **kwargs):
+#         dossier = Dossier.objects.get(user=request.user)
+#         serializers = DossierSerializers(dossier, data=request.data)
+#         if serializers.is_valid():
+#             serializers.save()
+#             return Response({"Update successful!!!"})
+#         return Response(serializers.errors)
+#
+#     def delete(self, request, *args, **kwargs):
+#         dossier = Dossier.objects.get(user=request.user)
+#         dossier.delete()
+#         return Response({"data": "OK"})
+
+
 class RegisterModelViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = RegisterSerializers
+
+
+class AuthView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
