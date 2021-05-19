02ac69a49aca47191994afeee0eab7c58adc25a3
risification <sultangaziev01@bk.ru>
2021-05-19 20:03:38 +0600

    delete router dossiers add dossier url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 router = DefaultRouter()
-router.register('dossiers', DossierModelViewSet, basename='dossier')
 router.register('register', RegisterModelViewSet)
 urlpatterns = [
     path('', include(router.urls), name='start'),
     path('login/', AuthView.as_view(), name='login'),
-    # path('dossier_delete/', DossierView.as_view())
+    path('dossier/', DossierModelViewSet.as_view(), name='dossier')
 ]
