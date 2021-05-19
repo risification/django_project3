0ce021ced770c3140a4d7b44a2ba28bbd014978a
risification <sultangaziev01@bk.ru>
2021-05-19 21:18:07 +0600

    change DossierPostMethod to RegisterPermission and request.method != "GET"

--- accounts/permission.py
+++ accounts/permission.py
@@ -1,7 +1,10 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class DossierPostMethod(BasePermission):
+from rest_framework.permissions import BasePermission,SAFE_METHODS
+
+
+class RegisterPermission(BasePermission):
     def has_permission(self, request, view):
-        if request.method == 'POST':
+        if request.method != 'GET':
             return True
