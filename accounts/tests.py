13c5aaec18fef5b94389ef8d38710e1e39925e3b
risification <sultangaziev01@bk.ru>
2021-05-21 18:02:23 +0600

    add Car objects and add self.response format='json'

--- accounts/tests.py
+++ accounts/tests.py
@@ -47,23 +47,37 @@ class Test_Dossier_Request(TestCase):
         self.dossier = Dossier.objects.create(full_name='ruslan sultangaziev', date_birth='2020-02-20', image=None,
                                               gender="M",
                                               user=self.user)
+        Car.objects.create(dossier=self.dossier, mark='check_mark', model='check_model', year='2021-02-02', number=1,
+                           color='color_check', type='mazda')
 
     def test_dossier_put(self):
         self.client.login(username='ruslan', password='12345678')
         data = {
-            "full_name": "ruslan sultangaziev112",
+            "id": 17,
+            "full_name": "Aliy Nurlanova",
             "date_birth": "2021-05-19",
             "gender": "M",
             "user": 1,
-            "car": [],
+            "car": [
+                {
+                    "car_id": 1,
+                    "mark": "mark",
+                    "model": "model",
+                    "year": "2021-05-21",
+                    "number": 2,
+                    "color": "color",
+                    "type": "mazda"
+                }
+            ],
             "education": [],
             "warcraft": []
         }
-        self.response = self.client.put(self.url, data)
+        self.response = self.client.put(self.url, data, format='json')
+        print(self.response.json())
         self.assertEqual(self.response.status_code, status.HTTP_200_OK)
 
     def test_delete(self):
         self.client.login(username='ruslan', password='12345678')
         self.response = self.client.delete(self.url)
         print(self.response.json())
-        self.assertEqual(self.response.status_code,status.HTTP_200_OK)
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
